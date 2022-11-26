a4fd849a099cbad66e383dc9f890205cfeb229e2
jiangxicheng <jiangxicheng@msunhealth.com>
2022-11-26 17:33:54 +0800

    init

---
+++ src/test/kotlin/org/jxch/crawler/alipay/MainPageTest.kt
@@ -0,0 +1,57 @@
+package org.jxch.crawler.alipay
+
+import com.codeborne.selenide.Condition.attribute
+import com.codeborne.selenide.Condition.visible
+import com.codeborne.selenide.Configuration
+import com.codeborne.selenide.Selectors.*
+import com.codeborne.selenide.Selenide
+import com.codeborne.selenide.Selenide.element
+import com.codeborne.selenide.Selenide.open
+import com.codeborne.selenide.logevents.SelenideLogger
+import io.qameta.allure.selenide.AllureSelenide
+import org.testng.annotations.*
+import org.testng.Assert.*
+
+class MainPageTest {
+    private val mainPage = MainPage()
+
+    companion object {
+        @BeforeClass
+        fun setUpAll() {
+            Configuration.browserSize = "1280x800"
+            SelenideLogger.addListener("allure", AllureSelenide())
+        }
+    }
+
+    @BeforeMethod
+    fun setUp() {
+        open("https://www.jetbrains.com/")
+    }
+
+    @Test
+    fun search() {
+        mainPage.searchButton.click()
+
+        element("[data-test='search-input']").sendKeys("Selenium")
+        element("button[data-test='full-search-button']").click()
+
+        element("input[data-test='search-input']").shouldHave(attribute("value", "Selenium"))
+    }
+
+    @Test
+    fun toolsMenu() {
+        mainPage.toolsMenu.click()
+
+        element("div[data-test='main-submenu']").shouldBe(visible)
+    }
+
+    @Test
+    fun navigationToAllTools() {
+        mainPage.seeDeveloperToolsButton.click()
+        mainPage.findYourToolsButton.click()
+
+        element("#products-page").shouldBe(visible)
+
+        assertEquals(Selenide.title(), "All Developer Tools and Products by JetBrains")
+    }
+}
